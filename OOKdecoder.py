a98e0abb900135ad66d2c450733624c9267bb1ca
Mansoor Almaleki <106423252+MansoorAlmaleki@users.noreply.github.com>
2026-07-01 10:48:09 +0400

    Update OOKdecoder.py

--- OOKdecoder.py
+++ OOKdecoder.py
@@ -3,16 +3,14 @@ import pygame
 import sys
 from scipy.io import wavfile
 #the transmitter uses a fixed bit width on-off keying encoding method
-#audio file crop must be alligned with the first bit of the preamble in order to work and a little longer than the postamble to make sure data does
-#not get cropped out
-
+#audio file left crop must be alligned with the start of the first preamble bit in order to work
 sample_rate, audio_data = wavfile.read('AudioData/25msBitWidth32x32BinaryImageA.wav')
 bit_width = 0.025
 samples_per_bit = bit_width * sample_rate
 image_size = 32*32
 preamble_size = 8
 postamble_size = 8
-total_samples_of_data = samples_per_bit*(image_size+preamble_size+postamble_size)#this is a constant where the data is 1024 bits plus 16 for preamble and postamble
+total_samples_of_data = samples_per_bit*(image_size+preamble_size+postamble_size)
 threshold = 0.075#tune this value, this is the bit detection threshold which checks if the intensity is greater than or equal to threshold to append it as an on or off
 
 BinaryData = []
@@ -20,7 +18,7 @@ Index = 0
 ProcessingAudio = True
 
 float_data = audio_data.astype(np.float32)
-abs_data = np.abs(float_data)
+abs_data = np.abs(float_data)#absolute the data
 normalized_data = abs_data/np.max(abs_data)
 
 while ProcessingAudio:
@@ -36,7 +34,7 @@ while ProcessingAudio:
         break
     Index += samples_per_bit
 
-BinaryData = BinaryData[preamble_size:-postamble_size]#remove the preamble and the postamble so the only data left is the image data
+BinaryData = BinaryData[preamble_size:-postamble_size]#remove the preamble and the postamble so the only data left is the image data(slicing)
 
 pygame.init()
 screen = pygame.display.set_mode((704, 704))
